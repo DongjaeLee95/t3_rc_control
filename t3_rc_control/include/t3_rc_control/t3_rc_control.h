380757d0e67c0bedbc303886170669441e0f5344
dongjae Lee <42236098+djLee-icsl@users.noreply.github.com>
2018-08-09 18:24:34 +0900

    Update t3_rc_control.h

--- t3_rc_control/include/t3_rc_control/t3_rc_control.h
+++ t3_rc_control/include/t3_rc_control/t3_rc_control.h
@@ -4,7 +4,7 @@
 #include <std_msgs/String.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/JointState.h>
-#include <geometry_msgs/Quaternion.h>
+// #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/Vector3.h>
 #include <tf/tf.h>
 
